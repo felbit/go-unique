738cefd7eae22e625699891bfb388ef88c817fa9
Martin Penckert <martin.penckert@ilpvfx.com>
2023-03-23 10:59:16 +0100

    Add generic typing

--- README.md
+++ README.md
@@ -1,32 +1,51 @@
 # goniq
+![Go Version](https://img.shields.io/badge/Go-v1.20-blue)
+![Coverage](https://img.shields.io/badge/coverage-100%25-brightgreen)
+
 *go unique* ... simple set-like operations for Go slices.
 
-:warning: This library is not considered stable. Therefore caution is advised using it for production right now.
+>   :warning: This library is not considered stable. Therefore, caution is advised using it for production for now.
 
-GoUnique is a Go language package that provides functions to add and remove elements from a (sorted) string slice. The package contains the functions `Add` and `Remove`.
+Goniq is a Go language package that provides functions to add and remove elements from a (sorted) string slice.
+
+## Goals
+
+- "out of the way": using this package should be intuitively and not require reading documentation
+- generic implementation: where-ever possible, users should not have to care about the types of things
+- idempotent behavior: running the same operation multiple times should not change the result (this is currently violated by the behavior of `Remove` on slices with non-unique entries)
+- independent: no use of external libraries
+- resource conscious: use as little resources as possible
 
 ## Functions
 
 ### Add
 
 ```go
-func Add([]string, string) []string
+func Add[T Ordered]([]T, T) []T
 ```
 
-:warning: `Add` currently returns a **sorted slice**. Event if the elements do not change, the slice will be modified!
+>   :warning: `Add` currently returns a **sorted slice**, even if the elements do not change. This might change in the future.
 
-This function takes a string slice and a string as input, and returns a new sorted string slice with the input string added to it. If the input slice is empty, it creates a new slice with only the input string. If the input string is already in the slice, it returns the original slice without modifying it. Otherwise, it uses the sort.SearchStrings function from the standard library to find the position where the input string should be inserted, and inserts it using the append function.
+This function takes a slice of T and an element of T as input,
+and returns a new sorted slice of T with the input element added to it.
+If the input slice is empty, it creates a new slice with only the input string.
+If the input string is already in the slice, it returns the original slice, but sorted.
 
 
 ### Remove
 
 ```go
-func Remove([]string, string) []string
+func Remove[T Ordered]([]T, T) []T
 ```
 
-:warning: `Remove` currently returns a **sorted slice**. Event if the elements do not change, the slice will be modified!
+>   :warning: `Remove` currently returns a **sorted slice**, even if the elements fo not change. This might change in the future.
 
-This function takes a string slice and a string as input, and returns a new sorted string slice with the input string removed from it. If the input slice is empty or the input string is not in the slice, it returns the original slice it. Otherwise, it uses the sort.SearchStrings function to find the position of the input string in the slice, and removes it using the copy function.
+>   :warning: Currently, only removes one of the entries, if there are multiple entries that match the input element! This will change in the future.
+
+This function takes a slice of T and an element of T as input,
+and returns a new sorted slice of T with the input element removed from it.
+If the input slice is empty or the input element is not in the slice,
+it returns the original slice sorted.
 
 ## Usage
 
@@ -44,5 +63,11 @@ func main() {
 	s = goniq.Add(s, "date") // returns ["apple", "banana", "cherry", "date"]
 	s = goniq.Remove(s, "banana") // returns ["apple", "cherry", "date"]
 	fmt.Println(s)
+
+	x := []int{3, 7, 2, 4}
+	x = goniq.Add(x, 7) // returns [2, 3, 4, 7]
+	x = goniq.Add(x, 5) // returns [2, 3, 4, 5, 7]
+	x = goniq.Remove(x, 3) // returns [2, 4, 5, 7]
+	fmt.Println(x)
 }
 ```
--- goniq_test.go
+++ goniq_test.go
@@ -7,9 +7,11 @@ import (
 	"github.com/felbit/goniq"
 )
 
-func TestAdd(t *testing.T) {
-	tests := []struct {
-		name    string
+// TODO: Use property based testing to test all possible types
+
+func TestAddString(t *testing.T) {
+	testCases := []struct {
+		title   string
 		slice   []string
 		element string
 		want    []string
@@ -20,19 +22,88 @@ func TestAdd(t *testing.T) {
 		{"add element that already exists in slice", []string{"bar", "baz", "foo"}, "foo", []string{"bar", "baz", "foo"}},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := goniq.Add(tt.slice, tt.element)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tt.want)
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Add(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestAddInt(t *testing.T) {
+	testCases := []struct {
+		title   string
+		slice   []int
+		element int
+		want    []int
+	}{
+		{"add to empty slice", []int{}, 1, []int{1}},
+		{"add to non-empty slice", []int{1, 2}, 3, []int{1, 2, 3}},
+		{"add duplicate element", []int{1, 2}, 2, []int{1, 2}},
+		{"add element that already exists in slice", []int{2, 3, 1}, 2, []int{1, 2, 3}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Add(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestAddUint(t *testing.T) {
+	testCases := []struct {
+		title   string
+		slice   []uint
+		element uint
+		want    []uint
+	}{
+		{"add to empty slice", []uint{}, 1, []uint{1}},
+		{"add to non-empty slice", []uint{1, 2}, 3, []uint{1, 2, 3}},
+		{"add duplicate element", []uint{1, 2}, 2, []uint{1, 2}},
+		{"add element that already exists in slice", []uint{2, 3, 1}, 2, []uint{1, 2, 3}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Add(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestAddFloat(t *testing.T) {
+	testCases := []struct {
+		title   string
+		slice   []float32
+		element float32
+		want    []float32
+	}{
+		{"add to empty slice", []float32{}, 1.3, []float32{1.3}},
+		{"add to non-empty slice", []float32{1.2, 2.3}, 3.4, []float32{1.2, 2.3, 3.4}},
+		{"add duplicate element", []float32{1.2, 2.3}, 2.3, []float32{1.2, 2.3}},
+		{"add element that already exists in slice", []float32{2.4, 2.3, 2.1}, 2.3, []float32{2.1, 2.3, 2.4}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Add(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("addUnique() = %v, want %v", got, tc.want)
 			}
 		})
 	}
 }
 
 func TestRemove(t *testing.T) {
-	tests := []struct {
-		name    string
+	testCases := []struct {
+		title   string
 		slice   []string
 		element string
 		want    []string
@@ -43,11 +114,80 @@ func TestRemove(t *testing.T) {
 		{"remove last element", []string{"bar", "baz", "foo"}, "foo", []string{"bar", "baz"}},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := goniq.Remove(tt.slice, tt.element)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("remove() = %v, want %v", got, tt.want)
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Remove(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("remove() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestRemoveInt(t *testing.T) {
+	testCases := []struct {
+		title   string
+		slice   []int
+		element int
+		want    []int
+	}{
+		{"remove from empty slice", []int{}, 1, []int{}},
+		{"remove from non-empty slice", []int{1, 2, 3}, 2, []int{1, 3}},
+		{"remove non-existing element", []int{1, 2}, 3, []int{1, 2}},
+		{"remove last element", []int{1, 2, 3}, 3, []int{1, 2}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Remove(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestRemoveUint(t *testing.T) {
+	testCases := []struct {
+		title   string
+		slice   []uint
+		element uint
+		want    []uint
+	}{
+		{"remove from empty slice", []uint{}, 1, []uint{}},
+		{"remove from non-empty slice", []uint{1, 2, 3}, 2, []uint{1, 3}},
+		{"remove non-existing element", []uint{1, 2}, 3, []uint{1, 2}},
+		{"remove last element", []uint{1, 2, 3}, 3, []uint{1, 2}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Remove(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestRemoveFloat(t *testing.T) {
+	testCases := []struct {
+		title   string
+		slice   []float32
+		element float32
+		want    []float32
+	}{
+		{"remove from empty slice", []float32{}, 0.1, []float32{}},
+		{"remove from non-empty slice", []float32{0.1, 0.2, -3}, 0.2, []float32{-3, 0.1}},
+		{"remove non-existing element", []float32{0.1, 0.2}, 0.19, []float32{0.1, 0.2}},
+		{"remove last element", []float32{1.1, 2.2, 3.3}, 3.3, []float32{1.1, 2.2}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.Remove(tc.slice, tc.element)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("addUnique() = %v, want %v", got, tc.want)
 			}
 		})
 	}
