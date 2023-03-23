a1c6820f94a031967a199de694fa387aa43e5673
Martin Penckert <martin.penckert@ilpvfx.com>
2023-03-23 14:33:13 +0100

    Add `NewSet` function to de-duplicate slice elements

--- goniq_test.go
+++ goniq_test.go
@@ -4,17 +4,61 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/felbit/goniq"
 )
 
 // TODO: Use property based testing to test all possible types
+
+func TestNewSetString(t *testing.T) {
+	testCases := []struct {
+		title  string
+		slice  []string
+		expect []string
+	}{
+		{"empty slice", []string{}, []string{}},
+		{"1 elem slice", []string{"foo"}, []string{"foo"}},
+		{"2 distinct elem slice", []string{"foo", "bar"}, []string{"foo", "bar"}},
+		{"multiple duplicates", []string{"foo", "bar", "foo", "bar", "quuz"}, []string{"foo", "bar", "quuz"}},
+		{"preserve original order", []string{"quuz", "foo", "quuz", "bar", "foo"}, []string{"quuz", "foo", "bar"}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.NewSet(tc.slice)
+			assert.Equal(t, tc.expect, got)
+		})
+	}
+}
+
+func TestNewSetInt(t *testing.T) {
+	testCases := []struct {
+		title  string
+		slice  []int
+		expect []int
+	}{
+		{"empty slice", []int{}, []int{}},
+		{"1 elem slice", []int{5}, []int{5}},
+		{"2 distinct elem slice", []int{2, 5}, []int{2, 5}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.title, func(t *testing.T) {
+			got := goniq.NewSet(tc.slice)
+			if !reflect.DeepEqual(tc.slice, tc.expect) {
+				t.Errorf("NewSet(): got %v, expected %v", got, tc.expect)
+			}
+		})
+	}
+}
 
 func TestAddString(t *testing.T) {
 	testCases := []struct {
 		title   string
 		slice   []string
 		element string
-		want    []string
+		expect  []string
 	}{
 		{"add to empty slice", []string{}, "foo", []string{"foo"}},
 		{"add to non-empty slice", []string{"bar", "baz"}, "foo", []string{"bar", "baz", "foo"}},
@@ -25,8 +69,8 @@ func TestAddString(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Add(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Add(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
@@ -37,7 +81,7 @@ func TestAddInt(t *testing.T) {
 		title   string
 		slice   []int
 		element int
-		want    []int
+		expect  []int
 	}{
 		{"add to empty slice", []int{}, 1, []int{1}},
 		{"add to non-empty slice", []int{1, 2}, 3, []int{1, 2, 3}},
@@ -48,8 +92,8 @@ func TestAddInt(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Add(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Add(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
@@ -60,7 +104,7 @@ func TestAddUint(t *testing.T) {
 		title   string
 		slice   []uint
 		element uint
-		want    []uint
+		expect  []uint
 	}{
 		{"add to empty slice", []uint{}, 1, []uint{1}},
 		{"add to non-empty slice", []uint{1, 2}, 3, []uint{1, 2, 3}},
@@ -71,8 +115,8 @@ func TestAddUint(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Add(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Add(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
@@ -83,7 +127,7 @@ func TestAddFloat(t *testing.T) {
 		title   string
 		slice   []float32
 		element float32
-		want    []float32
+		expect  []float32
 	}{
 		{"add to empty slice", []float32{}, 1.3, []float32{1.3}},
 		{"add to non-empty slice", []float32{1.2, 2.3}, 3.4, []float32{1.2, 2.3, 3.4}},
@@ -94,8 +138,8 @@ func TestAddFloat(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Add(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Add(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
@@ -106,7 +150,7 @@ func TestRemove(t *testing.T) {
 		title   string
 		slice   []string
 		element string
-		want    []string
+		expect  []string
 	}{
 		{"remove from empty slice", []string{}, "foo", []string{}},
 		{"remove from non-empty slice", []string{"bar", "baz", "foo"}, "baz", []string{"bar", "foo"}},
@@ -117,8 +161,8 @@ func TestRemove(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Remove(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("remove() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Remove(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
@@ -129,7 +173,7 @@ func TestRemoveInt(t *testing.T) {
 		title   string
 		slice   []int
 		element int
-		want    []int
+		expect  []int
 	}{
 		{"remove from empty slice", []int{}, 1, []int{}},
 		{"remove from non-empty slice", []int{1, 2, 3}, 2, []int{1, 3}},
@@ -140,8 +184,8 @@ func TestRemoveInt(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Remove(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Remove(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
@@ -152,7 +196,7 @@ func TestRemoveUint(t *testing.T) {
 		title   string
 		slice   []uint
 		element uint
-		want    []uint
+		expect  []uint
 	}{
 		{"remove from empty slice", []uint{}, 1, []uint{}},
 		{"remove from non-empty slice", []uint{1, 2, 3}, 2, []uint{1, 3}},
@@ -163,8 +207,8 @@ func TestRemoveUint(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Remove(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Remove(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
@@ -175,7 +219,7 @@ func TestRemoveFloat(t *testing.T) {
 		title   string
 		slice   []float32
 		element float32
-		want    []float32
+		expect  []float32
 	}{
 		{"remove from empty slice", []float32{}, 0.1, []float32{}},
 		{"remove from non-empty slice", []float32{0.1, 0.2, -3}, 0.2, []float32{-3, 0.1}},
@@ -186,8 +230,8 @@ func TestRemoveFloat(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.title, func(t *testing.T) {
 			got := goniq.Remove(tc.slice, tc.element)
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("addUnique() = %v, want %v", got, tc.want)
+			if !reflect.DeepEqual(got, tc.expect) {
+				t.Errorf("Remove(): got %v, expect %v", got, tc.expect)
 			}
 		})
 	}
