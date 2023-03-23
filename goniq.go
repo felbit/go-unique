84803b0fd2a3f2f9810678c1840cfc3152e65e21
Martin Penckert <martin.penckert@ilpvfx.com>
2023-03-23 10:59:16 +0100

    Add generic typing

--- goniq.go
+++ goniq.go
@@ -2,31 +2,51 @@ package goniq
 
 import "sort"
 
-func sortStringSlice(s []string) []string {
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type Int interface {
+	Signed | Unsigned
+}
+
+type Float interface {
+	~float32 | ~float64
+}
+
+type Ordered interface {
+	Float | Int | ~string
+}
+
+func sortSlice[T Ordered](s []T) []T {
 	sort.Slice(s, func(i, j int) bool { return s[i] < s[j] })
 	return s
 }
 
-func Add(slice []string, s string) []string {
+func Add[T Ordered](slice []T, s T) []T {
 	if len(slice) == 0 {
-		return []string{s}
+		return []T{s}
 	}
 
-	slice = sortStringSlice(slice)
-	idx := sort.SearchStrings(slice, s)
+	slice = sortSlice(slice)
+	idx := sort.Search(len(slice), func(i int) bool { return slice[i] >= s })
 	if idx == len(slice) || slice[idx] != s {
-		return append(slice[:idx], append([]string{s}, slice[idx:]...)...)
+		return append(slice[:idx], append([]T{s}, slice[idx:]...)...)
 	}
 	return slice
 }
 
-func Remove(slice []string, s string) []string {
+func Remove[T Ordered](slice []T, s T) []T {
 	if len(slice) == 0 {
 		return slice
 	}
 
-	slice = sortStringSlice(slice)
-	idx := sort.SearchStrings(slice, s)
+	slice = sortSlice(slice)
+	idx := sort.Search(len(slice), func(i int) bool { return slice[i] >= s })
 	if idx < len(slice) && slice[idx] == s {
 		if idx == len(slice)-1 {
 			return slice[:idx]
